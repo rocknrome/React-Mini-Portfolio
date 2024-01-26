ed75054e1b64eea4710241f9bf400e8038920063
Roman Larionov <pcsaveddotcom@gmail.com>
2024-01-25 20:52:44 -0500

    header styling applied

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Main from './components/Main'
 import Header from './components/header'
 import Footer from './components/Footer'
 import Projects from './components/Projects'
+/*import './index.css';*/
 
 function App() {
   return (
     <div className="App">
-      
+
       <Header />
       <Main />
       <Projects />
